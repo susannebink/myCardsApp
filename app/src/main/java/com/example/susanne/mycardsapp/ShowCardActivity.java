80ee62ea5bcfc84459f37e55a7b5e481deee506d
susannebink <s.binkhorst96@gmail.com>
2018-01-22 14:02:31 +0100

    Begin van de map werkt

--- app/src/main/java/com/example/susanne/mycardsapp/ShowCardActivity.java
+++ app/src/main/java/com/example/susanne/mycardsapp/ShowCardActivity.java
@@ -1,15 +1,23 @@
 package com.example.susanne.mycardsapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,11 +29,14 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 
-public class ShowCardActivity extends AppCompatActivity {
+public class ShowCardActivity extends AppCompatActivity implements OnMapReadyCallback{
     DatabaseReference databaseReference;
     FirebaseAuth mAuth;
     ImageView barcode;
     TextView cardNumber;
+    GoogleMap mMap;
+    private FusedLocationProviderClient mClient;
+    private Boolean locationPermission = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +53,10 @@ public class ShowCardActivity extends AppCompatActivity {
         String store = getIntent().getStringExtra("store");
         storeName.setText(store);
         getStoreCard(store);
+
+//        serviceOK();
+        getLocationPermission();
+
     }
 
     public void getStoreCard(final String name){
@@ -99,5 +114,74 @@ public class ShowCardActivity extends AppCompatActivity {
             }
         }
         return barcodeBitmap;
+    }
+
+//    public boolean serviceOK(){
+//        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(ShowCardActivity.this);
+//
+//        if (available == ConnectionResult.SUCCESS){
+//            return true;
+//        }
+//        else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+//            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(ShowCardActivity.this, available, ERROR_DIALOG_REQUEST);
+//            dialog.show();
+//        }
+//        else {
+//            Toast.makeText(ShowCardActivity.this, "Kan geen verzoeken doen voor map", Toast.LENGTH_LONG).show();
+//        }
+//        return false;
+//    }
+
+    private void getLocationPermission(){
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED){
+                locationPermission = true;
+                initMap();
+            }
+            else {
+                ActivityCompat.requestPermissions(ShowCardActivity.this, permissions, 1111);
+            }
+        }
+        else {
+            ActivityCompat.requestPermissions(ShowCardActivity.this, permissions, 1111);
+        }
+    }
+    private void getUserLocation(){
+        mClient = LocationServices.getFusedLocationClient(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        locationPermission = false;
+        switch (requestCode){
+            case 1111:{
+                if (grantResults.length > 0){
+                    for (int i=0; i < grantResults.length; i++){
+                        if (grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                            locationPermission = false;
+                            return;
+                        }
+                    }
+                    locationPermission = true;
+                    initMap();
+                }
+            }
+        }
+    }
+
+    private void initMap(){
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.myMap);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
     }
 }
